135dbe91c19d0983e127d3ca790e05431647d84b
Rawann0m <raawannm@gmail.com>
2025-05-04 18:40:01 +0300

    first commit

---
+++ EcoNest/EcoNest/EcoNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoNestApp.swift
+//  EcoNest
+//
+//  Created by Rawan on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EcoNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
