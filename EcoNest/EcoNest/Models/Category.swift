d9b18f0dff6ee31c0049288af749529a6561e525
Naif Ghannam <dhdfaui@gmail.com>
2025-05-13 09:33:13 +0300

    add categorytype

--- EcoNest/EcoNest/Models/Category.swift
+++ EcoNest/EcoNest/Models/Category.swift
@@ -5,11 +5,12 @@
 //  Created by Mac on 08/11/1446 AH.
 //
 
-
 import Foundation
 
-struct Category: Identifiable {
-    let id = UUID()
-    let name: String
-    let imageName: String
+// Represents a specific plant that belongs to a CategoryType
+struct Category: Identifiable, Codable {
+    var id: String     // Unique ID for this category
+    var name: String                       // Name of the plant (e.g., "Anthurium")
+    var imageName: String                  // Name of the image asset for the plant
+    var typeId: String                     // Foreign key: links to the CategoryType ID
 }
